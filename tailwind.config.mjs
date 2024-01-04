79af47e26faacb6bc3da43bfa3d99c47d1e15848
Luis Ernesto Hernandez Rivera <contacto@lernesto.com>
2024-01-03 18:39:09 -0600

    instalación de fuente Dangrek y colores base

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'blue-1' : '#1A2C32',
+				'blue-2' : '#F2F9F9'
+			}
+		},
 	},
 	plugins: [],
 }
